2a40f2547e0b30e3996d2ae2ce6f3255c53b2e4a
delower-hasan <dhhasansaha11@gmail.com>
2021-08-11 19:57:58 +0600

    File uploaded

--- js/script.js
+++ js/script.js
@@ -16,21 +16,21 @@ $('.catalog-wrapper').slick({
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 4,
+          slidesToScroll: 4,
           infinite: true,
           dots: true
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 768,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3
         }
       },
       {
-        breakpoint: 400,
+        breakpoint: 600,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2
@@ -60,6 +60,39 @@ $('.brand-wrapper').owlCarousel({
         }
     }
 })
+//   Item section
+$('.item-wrapper').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        992:{
+            items:4
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+// Product zoom
+
+$('.zoom-img').picZoomer();
+$('.menu-img').on('click',function(event){
+
+    var $pic = $(this).find('img');
+    
+    $('.zoom-img img').attr('src',$pic.attr('src'));
+  
+  });
+  
 
 
 
